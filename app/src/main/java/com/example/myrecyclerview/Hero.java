5d043538cfef591da8179d6fcabb9ac9db0a1223
Achmadsetiawan <achmadsetiawannn@gmail.com>
2019-09-17 00:02:35 +0700

    MyRecylerView V.1 Mode ListView

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.java
@@ -0,0 +1,68 @@
+package com.example.myrecyclerview;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Hero implements Parcelable {
+    private String name;
+    private String from;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    private String photo;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.from);
+        dest.writeString(this.photo);
+    }
+
+    public Hero() {
+    }
+
+    protected Hero(Parcel in) {
+        this.name = in.readString();
+        this.from = in.readString();
+        this.photo = in.readString();
+    }
+
+    public static final Parcelable.Creator<Hero> CREATOR = new Parcelable.Creator<Hero>() {
+        @Override
+        public Hero createFromParcel(Parcel source) {
+            return new Hero(source);
+        }
+
+        @Override
+        public Hero[] newArray(int size) {
+            return new Hero[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+
+    Structure :
+    activity_main.xml => item_row_hero.xml
+    Hero.java => HeroesData.java =>ListHeroAdapter.java
+
+
+
+    */
+
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+
+
+
+}
